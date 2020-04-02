3a518bbd3e1333912de0f9d05d439c5969b2b07c
Brais Cidras <braisomary@gmail.com>
2020-04-02 15:15:21 +0200

    Building a Repository

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,29 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Repository for fetching devbyte videos from the network and storing them on disk.
+ */
+class VideosRepository(private val database: VideosDatabase) {
+
+    // Refreshing the offline cache
+    // It's a suspend function because will be called from a co-routine
+    suspend fun refreshVideos() {
+        // Forcing the Kotlin co-routine to switch to the IO dispatcher
+        // In this way refreshVideos() is safety called from any dispatcher, even the main thread
+        withContext(Dispatchers.IO) {
+            // With await we are telling the co-routine to suspend until the data is available
+            val playlist = Network.devbytes.getPlaylist().await()
+            // Note the asterisk * is the spread operator. It allows us to pass in an array to a function that expects varargs.
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+
+}
